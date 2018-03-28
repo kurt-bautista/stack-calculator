6068ae7bc5c333631c81d53d3a846c9c8d6d0d03
Kurt Vincent Bautista <kurt.bautista@outlook.com>
2018-03-28 12:15:12 +0800

    Add switch for imperative implementation

--- calculator.py
+++ calculator.py
@@ -1,3 +1,18 @@
+import sys
+import argparse
 from object_oriented.stack import Stack
 
+parser = argparse.ArgumentParser()
+parser.add_argument(
+    '--imperative',
+    '-i',
+    help='Use imperative implementation of stack',
+    action='store_true')
+args = parser.parse_args(sys.argv[1:])
+
 expression = input("Enter postfix expression: ").split()
+
+if not args.imperative:
+    pass
+else:
+    pass
